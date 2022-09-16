6da65d5d09a122392ec7205d27ee3d22d807911f
ZuKerNo1 <trankimvu2002@gmail.com>
2022-09-16 18:57:36 +0700

    nop bt tuan 4

---
+++ BT4_DiChuyen2ManHinh/app/src/main/java/com/example/bt4_dichuyen2manhinh/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.bt4_dichuyen2manhinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
